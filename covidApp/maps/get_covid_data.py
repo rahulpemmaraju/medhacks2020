4cf99a4f61eca349373d82bb29a9a27e07cdd6fa
Rahul Pemmaraju <rahul@Rahuls-MacBook-Pro.local>
2020-09-05 10:50:50 -0400

    clean

---
+++ covidApp/maps/get_covid_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+'''daily_cases_url="https://enigmaforensics.com/covid-19/us/state/NewJersey_Cases_Delta_By_Date.csv"
+daily_deaths_url="https://enigmaforensics.com/covid-19/us/state/NewJersey_Deaths_Delta_By_Date.csv"
+
+case_frame=pd.read_csv(daily_cases_url)
+
+print(case_frame)'''
